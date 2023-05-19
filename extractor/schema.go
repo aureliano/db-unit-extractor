437cdd65a6ade302f63b958060b31be72936bbc9
Aureliano <aureliano.franca@hotmail.com>
2023-05-18 21:33:08 -0300

    Schema validation.

--- extractor/schema.go
+++ extractor/schema.go
@@ -1,22 +1,43 @@
 package extractor
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"regexp"
 
+	"github.com/aureliano/db-unit-extractor/dataconv"
 	"gopkg.in/yaml.v2"
 )
 
+const NameMaxLength = 80
+
+var ErrSchemaValidation = errors.New("validation")
+var nameRegExp = regexp.MustCompile(`^[a-zA-Z_]\w+$`)
+
+type ConverterSchema string
+type ColumnSchema string
+type IgnoreSchema string
+
+type FilterSchema struct {
+	Name  string `yaml:"name"`
+	Value string `yaml:"value"`
+}
+
+type TableSchema struct {
+	Name    string         `yaml:"name"`
+	Filters []FilterSchema `yaml:"filters"`
+	Columns []ColumnSchema `yaml:"columns"`
+	Ignore  []IgnoreSchema `yaml:"ignore"`
+}
+
 type Schema struct {
-	Converters []string `yaml:"converters"`
-	Tables     []struct {
-		Name    string `yaml:"name"`
-		Filters []struct {
-			Name  string
-			Value string
-		} `yaml:"filters"`
-		Columns []string `yaml:"columns"`
-		Ignore  []string `yaml:"ignore"`
-	} `yaml:"tables"`
+	Converters []ConverterSchema `yaml:"converters"`
+	Tables     []TableSchema     `yaml:"tables"`
+}
+
+type SchemaValidator interface {
+	Validate() error
 }
 
 func DigestSchema(fpath string) (Schema, error) {
@@ -27,9 +48,110 @@ func DigestSchema(fpath string) (Schema, error) {
 		return schema, err
 	}
 
-	if err := yaml.Unmarshal([]byte(yml), &schema); err != nil {
+	if err = yaml.Unmarshal(yml, &schema); err != nil {
 		return schema, err
 	}
 
 	return schema, nil
+}
+
+func (s Schema) Validate() error {
+	if len(s.Converters) > 0 {
+		if err := validateConverters(s.Converters); err != nil {
+			return err
+		}
+	}
+
+	return validateTables(s.Tables)
+}
+
+func (c ConverterSchema) Validate() error {
+	if !dataconv.ConverterExists(string(c)) {
+		return errors.Join(ErrSchemaValidation, fmt.Errorf("converter '%s' not found", c))
+	}
+
+	return nil
+}
+
+func (t TableSchema) Validate() error {
+	if err := validateName(t.Name); err != nil {
+		return err
+	}
+
+	for _, filter := range t.Filters {
+		if err := filter.Validate(); err != nil {
+			return errors.Join(fmt.Errorf("table '%s'", t.Name), err)
+		}
+	}
+
+	for _, column := range t.Columns {
+		if err := column.Validate(); err != nil {
+			return errors.Join(fmt.Errorf("table '%s'", t.Name), err)
+		}
+	}
+
+	for _, column := range t.Ignore {
+		if err := column.Validate(); err != nil {
+			return errors.Join(fmt.Errorf("table '%s'", t.Name), err)
+		}
+	}
+
+	if len(t.Columns) > 0 && len(t.Ignore) > 0 {
+		return errors.Join(ErrSchemaValidation,
+			fmt.Errorf("table '%s' with columns and ignore set (excludents)", t.Name))
+	}
+
+	return nil
+}
+
+func (f FilterSchema) Validate() error {
+	if len(f.Value) == 0 {
+		return errors.Join(ErrSchemaValidation, fmt.Errorf("empty filter value '%s'", f.Name))
+	}
+
+	return validateName(f.Name)
+}
+
+func (c ColumnSchema) Validate() error {
+	return validateName(string(c))
+}
+
+func (c IgnoreSchema) Validate() error {
+	return validateName(string(c))
+}
+
+func validateConverters(converters []ConverterSchema) error {
+	for _, converter := range converters {
+		if err := converter.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validateTables(tables []TableSchema) error {
+	if len(tables) == 0 {
+		return errors.Join(ErrSchemaValidation, errors.New("no table provided"))
+	}
+
+	for _, table := range tables {
+		if err := table.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validateName(name string) error {
+	if len(name) > NameMaxLength {
+		return errors.Join(ErrSchemaValidation, fmt.Errorf("'%s' invalid name size", name))
+	}
+
+	if !nameRegExp.MatchString(name) {
+		return errors.Join(ErrSchemaValidation, fmt.Errorf("'%s' invalid name", name))
+	}
+
+	return nil
 }
