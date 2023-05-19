3e14bb87a3dd034548d9f761e53edb942394b9bf
Aureliano <aureliano.franca@hotmail.com>
2023-05-19 19:26:16 -0300

    Tables classification by dependency.

--- extractor/schema.go
+++ extractor/schema.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 
 	"github.com/aureliano/db-unit-extractor/dataconv"
 	"gopkg.in/yaml.v2"
@@ -16,7 +17,7 @@ var (
 	ErrSchemaValidation    = errors.New("validation")
 	ErrTableClassification = errors.New("classification")
 	nameRegExp             = regexp.MustCompile(`^[a-zA-Z_]\w+$`)
-	filterReferenceRegExp  = regexp.MustCompile(`^\$\{\w+\.\w+\}$`)
+	filterReferenceRegExp  = regexp.MustCompile(`^\$\{(\w+)\.(\w+)\}$`)
 )
 
 type ConverterSchema string
@@ -138,6 +139,24 @@ func (s Schema) Classify() error {
 		s.Tables[indexes[i]].GroupID = 1
 	}
 
+	group := 2
+	for {
+		indexes, err = classifyGroupsButOne(s, group)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrTableClassification, err)
+		}
+
+		if len(indexes) == 0 {
+			break
+		}
+
+		for i := 0; i < len(indexes); i++ {
+			s.Tables[indexes[i]].GroupID = group
+		}
+
+		group++
+	}
+
 	return nil
 }
 
@@ -145,10 +164,19 @@ func classifyGroupOne(s Schema) ([]int, error) {
 	indexes := make([]int, 0, len(s.Tables))
 
 	for i, table := range s.Tables {
+		levelOne := false
+		referenced := false
+
 		for _, filter := range table.Filters {
-			if !filterReferenceRegExp.MatchString(filter.Value) {
-				indexes = append(indexes, i)
+			if filterReferenceRegExp.MatchString(filter.Value) {
+				referenced = true
+			} else {
+				levelOne = true
 			}
+		}
+
+		if len(table.Filters) == 0 || (levelOne && !referenced) {
+			indexes = append(indexes, i)
 		}
 	}
 
@@ -157,6 +185,42 @@ func classifyGroupOne(s Schema) ([]int, error) {
 	}
 
 	return indexes, nil
+}
+
+func classifyGroupsButOne(s Schema, group int) ([]int, error) {
+	indexes := make([]int, 0, len(s.Tables))
+
+	for i, table := range s.Tables {
+		for _, filter := range table.Filters {
+			matches := filterReferenceRegExp.FindAllStringSubmatch(filter.Value, -1)
+
+			if matches != nil {
+				refTable := matches[0][1]
+				index := findTableByName(s, refTable)
+
+				if index < 0 {
+					return nil, fmt.Errorf("%s.%s points to unresolvable reference '%s'", table.Name, filter.Name, matches[0][0])
+				}
+
+				if s.Tables[index].GroupID+1 == group {
+					indexes = append(indexes, i)
+				}
+			}
+		}
+	}
+
+	return indexes, nil
+}
+
+func findTableByName(s Schema, tname string) int {
+	for i, table := range s.Tables {
+		name := strings.ToLower(table.Name)
+		if tname == name {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func validateConverters(converters []ConverterSchema) error {
--- extractor/schema_test.go
+++ extractor/schema_test.go
@@ -273,6 +273,20 @@ func TestClassifyGroupOneNotClassified(t *testing.T) {
 
 	err := s.Classify()
 	assert.ErrorIs(t, err, extractor.ErrTableClassification)
+	assert.Contains(t, err.Error(), "couldn't find any level one tables")
+}
+
+func TestClassifyReferenceNotFound(t *testing.T) {
+	s := extractor.Schema{
+		Tables: []extractor.TableSchema{
+			{Name: "t1", Filters: []extractor.FilterSchema{{Name: "id", Value: "1"}}},
+			{Name: "t2", Filters: []extractor.FilterSchema{{Name: "id", Value: "${table.column}"}}},
+		},
+	}
+
+	err := s.Classify()
+	assert.ErrorIs(t, err, extractor.ErrTableClassification)
+	assert.Contains(t, err.Error(), "t2.id points to unresolvable reference '${table.column}'")
 }
 
 func TestClassify(t *testing.T) {
@@ -282,6 +296,21 @@ func TestClassify(t *testing.T) {
 	err = schema.Classify()
 	assert.Nil(t, err)
 
-	table := schema.Tables[2]
-	assert.Equal(t, 1, table.GroupID)
+	assert.Equal(t, 1, schema.Tables[1].GroupID)
+	assert.Equal(t, 1, schema.Tables[3].GroupID)
+	assert.Equal(t, 1, schema.Tables[7].GroupID)
+
+	assert.Equal(t, 2, schema.Tables[0].GroupID)
+	assert.Equal(t, 2, schema.Tables[5].GroupID)
+	assert.Equal(t, 2, schema.Tables[6].GroupID)
+	assert.Equal(t, 2, schema.Tables[8].GroupID)
+
+	assert.Equal(t, 3, schema.Tables[2].GroupID)
+	assert.Equal(t, 3, schema.Tables[9].GroupID)
+	assert.Equal(t, 3, schema.Tables[12].GroupID)
+
+	assert.Equal(t, 4, schema.Tables[10].GroupID)
+	assert.Equal(t, 4, schema.Tables[11].GroupID)
+
+	assert.Equal(t, 5, schema.Tables[4].GroupID)
 }
