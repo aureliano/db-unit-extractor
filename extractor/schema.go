0491ac7936d0631c298c302c8359852b69c2e208
Aureliano <aureliano.franca@hotmail.com>
2023-05-19 19:45:24 -0300

    4 group tables by dependency (#9)
    * Validates schema before returning it.
    
    * Tables classification by dependency.

--- extractor/schema.go
+++ extractor/schema.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 
 	"github.com/aureliano/db-unit-extractor/dataconv"
 	"gopkg.in/yaml.v2"
@@ -12,8 +13,12 @@ import (
 
 const NameMaxLength = 80
 
-var ErrSchemaValidation = errors.New("validation")
-var nameRegExp = regexp.MustCompile(`^[a-zA-Z_]\w+$`)
+var (
+	ErrSchemaValidation    = errors.New("validation")
+	ErrTableClassification = errors.New("classification")
+	nameRegExp             = regexp.MustCompile(`^[a-zA-Z_]\w+$`)
+	filterReferenceRegExp  = regexp.MustCompile(`^\$\{(\w+)\.(\w+)\}$`)
+)
 
 type ConverterSchema string
 type ColumnSchema string
@@ -25,6 +30,7 @@ type FilterSchema struct {
 }
 
 type TableSchema struct {
+	GroupID int
 	Name    string         `yaml:"name"`
 	Filters []FilterSchema `yaml:"filters"`
 	Columns []ColumnSchema `yaml:"columns"`
@@ -40,6 +46,10 @@ type SchemaValidator interface {
 	Validate() error
 }
 
+type GroupClassifier interface {
+	Classify() error
+}
+
 func DigestSchema(fpath string) (Schema, error) {
 	schema := Schema{}
 	yml, err := os.ReadFile(fpath)
@@ -52,7 +62,7 @@ func DigestSchema(fpath string) (Schema, error) {
 		return schema, err
 	}
 
-	return schema, nil
+	return schema, schema.Validate()
 }
 
 func (s Schema) Validate() error {
@@ -117,6 +127,100 @@ func (c ColumnSchema) Validate() error {
 
 func (c IgnoreSchema) Validate() error {
 	return validateName(string(c))
+}
+
+func (s Schema) Classify() error {
+	indexes, err := classifyGroupOne(s)
+	if err != nil {
+		return fmt.Errorf("%w: %w", ErrTableClassification, err)
+	}
+
+	for i := 0; i < len(indexes); i++ {
+		s.Tables[indexes[i]].GroupID = 1
+	}
+
+	group := 2
+	for {
+		indexes, err = classifyGroupsButOne(s, group)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrTableClassification, err)
+		}
+
+		if len(indexes) == 0 {
+			break
+		}
+
+		for i := 0; i < len(indexes); i++ {
+			s.Tables[indexes[i]].GroupID = group
+		}
+
+		group++
+	}
+
+	return nil
+}
+
+func classifyGroupOne(s Schema) ([]int, error) {
+	indexes := make([]int, 0, len(s.Tables))
+
+	for i, table := range s.Tables {
+		levelOne := false
+		referenced := false
+
+		for _, filter := range table.Filters {
+			if filterReferenceRegExp.MatchString(filter.Value) {
+				referenced = true
+			} else {
+				levelOne = true
+			}
+		}
+
+		if len(table.Filters) == 0 || (levelOne && !referenced) {
+			indexes = append(indexes, i)
+		}
+	}
+
+	if len(indexes) == 0 {
+		return indexes, fmt.Errorf("couldn't find any level one tables")
+	}
+
+	return indexes, nil
+}
+
+func classifyGroupsButOne(s Schema, group int) ([]int, error) {
+	indexes := make([]int, 0, len(s.Tables))
+
+	for i, table := range s.Tables {
+		for _, filter := range table.Filters {
+			matches := filterReferenceRegExp.FindAllStringSubmatch(filter.Value, -1)
+
+			if matches != nil {
+				refTable := matches[0][1]
+				index := findTableByName(s, refTable)
+
+				if index < 0 {
+					return nil, fmt.Errorf("%s.%s points to unresolvable reference '%s'", table.Name, filter.Name, matches[0][0])
+				}
+
+				if s.Tables[index].GroupID+1 == group {
+					indexes = append(indexes, i)
+				}
+			}
+		}
+	}
+
+	return indexes, nil
+}
+
+func findTableByName(s Schema, tname string) int {
+	for i, table := range s.Tables {
+		name := strings.ToLower(table.Name)
+		if tname == name {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func validateConverters(converters []ConverterSchema) error {
--- extractor/schema_test.go
+++ extractor/schema_test.go
@@ -26,6 +26,8 @@ func TestDigestSchemaUnmarshalError(t *testing.T) {
 }
 
 func TestDigestSchema(t *testing.T) {
+	dataconv.RegisterConverter("conv_date_time", DummyConverter(""))
+	dataconv.RegisterConverter("conv_timestamp", DummyConverter(""))
 	schema, err := extractor.DigestSchema("../test/unit/schema_test.yml")
 
 	require.Nil(t, err)
@@ -260,4 +262,55 @@ func TestIgnoreSchema(t *testing.T) {
 	err := s.Validate()
 	assert.ErrorIs(t, err, extractor.ErrSchemaValidation)
 	assert.Contains(t, err.Error(), "'' invalid name")
+}
+
+func TestClassifyGroupOneNotClassified(t *testing.T) {
+	s := extractor.Schema{
+		Tables: []extractor.TableSchema{
+			{Name: "t1", Filters: []extractor.FilterSchema{{Name: "id", Value: "${table.column}"}}},
+		},
+	}
+
+	err := s.Classify()
+	assert.ErrorIs(t, err, extractor.ErrTableClassification)
+	assert.Contains(t, err.Error(), "couldn't find any level one tables")
+}
+
+func TestClassifyReferenceNotFound(t *testing.T) {
+	s := extractor.Schema{
+		Tables: []extractor.TableSchema{
+			{Name: "t1", Filters: []extractor.FilterSchema{{Name: "id", Value: "1"}}},
+			{Name: "t2", Filters: []extractor.FilterSchema{{Name: "id", Value: "${table.column}"}}},
+		},
+	}
+
+	err := s.Classify()
+	assert.ErrorIs(t, err, extractor.ErrTableClassification)
+	assert.Contains(t, err.Error(), "t2.id points to unresolvable reference '${table.column}'")
+}
+
+func TestClassify(t *testing.T) {
+	schema, err := extractor.DigestSchema("../test/unit/schema_test_grouping.yml")
+	require.Nil(t, err)
+
+	err = schema.Classify()
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, schema.Tables[1].GroupID)
+	assert.Equal(t, 1, schema.Tables[3].GroupID)
+	assert.Equal(t, 1, schema.Tables[7].GroupID)
+
+	assert.Equal(t, 2, schema.Tables[0].GroupID)
+	assert.Equal(t, 2, schema.Tables[5].GroupID)
+	assert.Equal(t, 2, schema.Tables[6].GroupID)
+	assert.Equal(t, 2, schema.Tables[8].GroupID)
+
+	assert.Equal(t, 3, schema.Tables[2].GroupID)
+	assert.Equal(t, 3, schema.Tables[9].GroupID)
+	assert.Equal(t, 3, schema.Tables[12].GroupID)
+
+	assert.Equal(t, 4, schema.Tables[10].GroupID)
+	assert.Equal(t, 4, schema.Tables[11].GroupID)
+
+	assert.Equal(t, 5, schema.Tables[4].GroupID)
 }
