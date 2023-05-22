ec6fe823fbe54598af24ea61fb2628d1cdb80028
Aureliano <aureliano.franca@hotmail.com>
2023-05-22 17:28:14 -0300

    Multitask database reader. Launches a routine for each table in a group.

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -1,8 +1,8 @@
 package extractor
 
 import (
+	"errors"
 	"fmt"
-	"sync"
 
 	"github.com/aureliano/db-unit-extractor/reader"
 	"github.com/aureliano/db-unit-extractor/schema"
@@ -12,6 +12,15 @@ type Conf struct {
 	SchemaPath string
 	reader.DataSource
 }
+
+type dbResponse struct {
+	table   string
+	filters [][]interface{}
+	data    []map[string]interface{}
+	err     error
+}
+
+var ErrExtractor = errors.New("extractor")
 
 func Extract(conf Conf) error {
 	schema, err := schema.DigestSchema(conf.SchemaPath)
@@ -24,27 +33,75 @@ func Extract(conf Conf) error {
 		return err
 	}
 
-	return extract(conf.DataSource, schema, db)
+	return extract(schema, db)
 }
 
-func extract(ds reader.DataSource, schema schema.Model, db reader.DBReader) error {
-	groupedTables := schema.GroupedTables()
-	var wg sync.WaitGroup
+func extract(model schema.Model, db reader.DBReader) error {
+	references := prepareReferences(model)
+	respChan := make(chan dbResponse)
 
-	for _, tables := range groupedTables {
+	defer close(respChan)
+
+	for _, tables := range model.GroupedTables() {
+		responses := make([]dbResponse, 0, len(tables))
+
 		for _, table := range tables {
-			fmt.Println(table)
+			filters := resolveTableFilters(table, references)
+
+			go fetchData(respChan, table, db, convToStr(model.Converters), filters)
 		}
 
-		/*wg.Add(1)
-		go func(table TableSchema) {
-			defer wg.Done()
+		for res := range respChan {
+			if res.err != nil {
+				return fmt.Errorf("%w: %w", ErrExtractor, res.err)
+			}
 
-			db.FetchColumnsMetadata(table.Name, table.Columns, table.Ignore)
-		}(table)*/
+			responses = append(responses, res)
+		}
 	}
 
-	wg.Wait()
-
 	return nil
+}
+
+func fetchData(c chan dbResponse, table schema.Table,
+	db reader.DBReader, converters []string, filters [][]interface{}) {
+	columns, err := db.FetchColumnsMetadata(table)
+	if err != nil {
+		c <- dbResponse{err: err}
+		return
+	}
+
+	data, err := db.FetchData(table.Name, columns, converters, filters)
+	if err != nil {
+		c <- dbResponse{err: err}
+		return
+	}
+
+	c <- dbResponse{
+		table:   table.Name,
+		filters: updateReferences(data),
+		data:    data,
+		err:     err,
+	}
+}
+
+func updateReferences(_ []map[string]interface{}) [][]interface{} {
+	panic("unimplemented")
+}
+
+func resolveTableFilters(_ schema.Table, _ map[string]interface{}) [][]interface{} {
+	panic("unimplemented")
+}
+
+func prepareReferences(_ schema.Model) map[string]interface{} {
+	panic("unimplemented")
+}
+
+func convToStr(conv []schema.Converter) []string {
+	res := make([]string, len(conv))
+	for i, c := range conv {
+		res[i] = string(c)
+	}
+
+	return res
 }
