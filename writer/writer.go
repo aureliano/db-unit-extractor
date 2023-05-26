82d61d174ba5ee50d712d07c0d820b01e9629e9a
Aureliano <aureliano.franca@hotmail.com>
2023-05-26 16:41:08 -0300

    Dummy writer for testing purpose.

--- writer/writer.go
+++ writer/writer.go
@@ -6,10 +6,8 @@ import (
 )
 
 type FileConf struct {
-	Type                 string
-	Formatted            bool
-	TabSize              int
-	BreakLineAfterColumn bool
+	Type      string
+	Formatted bool
 }
 
 var ErrUnsupportedFileWriter = errors.New("unsupported file type")
@@ -20,6 +18,19 @@ type FileWriter interface {
 	Write(table string, rows []map[string]interface{}) error
 }
 
+type DummyWriter struct{}
+
+func (DummyWriter) WriteHeader() { _ = 0 }
+
+func (DummyWriter) WriteFooter() { _ = 0 }
+
+func (DummyWriter) Write(_ string, _ []map[string]interface{}) error {
+	return nil
+}
+
 func NewWriter(conf FileConf) (FileWriter, error) {
+	if conf.Type == "dummy" {
+		return DummyWriter{}, nil
+	}
 	return nil, fmt.Errorf("%w: %s", ErrUnsupportedFileWriter, conf.Type)
 }
