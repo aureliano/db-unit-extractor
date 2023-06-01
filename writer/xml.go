b166e8b0751fa5cea501dfbfde2ef80ef210cfb9
Aureliano <aureliano.franca@hotmail.com>
2023-06-01 16:04:23 -0300

    Merge incoming stash.

--- writer/xml.go
+++ writer/xml.go
@@ -1,8 +1,30 @@
 package writer
 
-type XMLWriter struct{}
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
-func (XMLWriter) WriteHeader() error {
+type XMLWriter struct {
+	Formatted bool
+	Directory string
+	Name      string
+	file      *os.File
+}
+
+func (w XMLWriter) WriteHeader() error {
+	err := os.MkdirAll(w.Directory, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	path := filepath.Join(w.Directory, fmt.Sprintf("%s.xml", w.Name))
+	w.file, err = os.Create(path)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
