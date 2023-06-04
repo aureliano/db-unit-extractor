69bdf585a7579730c8332222e9eaf3011b93e1df
Aureliano <aureliano.franca@hotmail.com>
2023-06-04 18:50:07 -0300

    Fixes container creation and wait until database is ready.

--- test/integration/oracle/config.sh
+++ test/integration/oracle/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+base_dir="$(cd "$(dirname "${0}")" ; pwd -P)"
+startup_path="${base_dir}/scripts/startup"
 container_name='db-unit-extractor'
 oracle_port='1521'
 emde_port='5500'
@@ -14,8 +16,10 @@ oracle_password='admin'
 #
 # Connect to Oracle via sqlplus within the container: docker exec -it $container_name sqlplus / as sysdba
 
-docker run -d --rm --name $container_name \
-  -p $oracle_port:1521 -p $emde_port:5500 \
-  -e ORACLE_PWD=$oracle_password \
-  -v ./scripts/startup:/opt/oracle/scripts/startup \
+docker run -d --rm --name ${container_name} \
+  -p ${oracle_port}:1521 -p ${emde_port}:5500 \
+  -e ORACLE_PWD=${oracle_password} \
+  -v ${startup_path}:/opt/oracle/scripts/startup \
   container-registry.oracle.com/database/express:21.3.0-xe
+
+( docker logs -f db-unit-extractor & ) | grep -q "DONE: Executing user defined scripts"
