4c11da8e596db931859ae4672ab34c7fe3776565
Aureliano <aureliano.franca@hotmail.com>
2023-06-11 17:53:01 -0300

    CPU profiler.

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/aureliano/db-unit-extractor/cmd"
 	_ "github.com/sijms/go-ora/v2"
 )
 
 func main() {
+	cpuProfile := os.Getenv("CPU_PROFILE")
+	if cpuProfile != "" {
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	err := cmd.NewRootCommand().Execute()
 	if err != nil {
 		os.Exit(1)
