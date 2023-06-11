1b9d9846cf137e84d80e0053a6814fa9e3c1107a
Aureliano <aureliano.franca@hotmail.com>
2023-06-11 18:44:46 -0300

    46 add support to program profiling (#51)
    * CPU profiler.
    
    * Memory profiler.
    
    * Code refactoring according to directives of linters.

--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/aureliano/db-unit-extractor/cmd"
 	_ "github.com/sijms/go-ora/v2"
 )
 
 func main() {
-	err := cmd.NewRootCommand().Execute()
+	cpuProfile := os.Getenv("CPU_PROFILE")
+	var file *os.File
+	var err error
+	if cpuProfile != "" {
+		file, err = os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err = pprof.StartCPUProfile(file); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	memProfile := os.Getenv("MEM_PROFILE")
+	if memProfile != "" {
+		file, err = os.Create(memProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err = pprof.WriteHeapProfile(file); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = cmd.NewRootCommand().Execute()
+	if cpuProfile != "" {
+		pprof.StopCPUProfile()
+	}
+
+	if file != nil {
+		_ = file.Close()
+	}
+
 	if err != nil {
 		os.Exit(1)
 	}
