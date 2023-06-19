63840bfc5877c597b43ffcc6029161fbacaa6003
Aureliano <aureliano.franca@hotmail.com>
2023-06-19 20:38:46 -0300

    72 simplify datetime conversion when column is date only (#76)
    * Simplifies date/time conversion when column is date only to yyyy-MM-dd.
    
    * Fix date/time expectations to show only date when time is zero.

--- dataconv/date_time_iso8601_converter.go
+++ dataconv/date_time_iso8601_converter.go
@@ -7,7 +7,10 @@ import (
 
 type DateTimeISO8601Converter struct{}
 
-const dateTimeISO8601Layout = "2006-01-02T15:04:05.999 -0700"
+const (
+	dateISO8601Layout     = "2006-01-02"
+	dateTimeISO8601Layout = "2006-01-02T15:04:05.999 -0700"
+)
 
 func (DateTimeISO8601Converter) Convert(source interface{}) (interface{}, error) {
 	tm, isTime := source.(time.Time)
@@ -15,10 +18,18 @@ func (DateTimeISO8601Converter) Convert(source interface{}) (interface{}, error)
 		return nil, fmt.Errorf("'%v' is not time.Time", source)
 	}
 
-	return tm.Format(dateTimeISO8601Layout), nil
+	if isDateAndTime(tm) {
+		return tm.Format(dateTimeISO8601Layout), nil
+	}
+
+	return tm.Format(dateISO8601Layout), nil
 }
 
 func (DateTimeISO8601Converter) Handle(vl interface{}) bool {
 	_, handled := vl.(time.Time)
 	return handled
+}
+
+func isDateAndTime(tm time.Time) bool {
+	return !(tm.Hour() == 0 && tm.Minute() == 0 && tm.Second() == 0)
 }
