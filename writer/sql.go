34113dd4eb546763fc80060145347f0ea7133578
Aureliano <aureliano.franca@hotmail.com>
2023-06-21 10:47:32 -0300

    Error test cases to header writing.

--- writer/sql.go
+++ writer/sql.go
@@ -1,6 +1,11 @@
 package writer
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 type SQLWriter struct {
 	Formatted bool
@@ -9,7 +14,22 @@ type SQLWriter struct {
 	file      *os.File
 }
 
-func (w *SQLWriter) WriteHeader() error { return nil }
+func (w *SQLWriter) WriteHeader() error {
+	err := os.MkdirAll(w.Directory, os.ModePerm)
+	if err != nil {
+		log.Printf("SQL.WriteHeader\nMake directory %s failed with `%s'\n", w.Directory, err.Error())
+		return err
+	}
+
+	path := filepath.Join(w.Directory, fmt.Sprintf("%s.sql", w.Name))
+	w.file, err = os.Create(path)
+	if err != nil {
+		log.Printf("SQL.WriteHeader\nFile %s not created: `%s'\n", path, err.Error())
+		return err
+	}
+
+	return nil
+}
 
 func (w *SQLWriter) WriteFooter() error { return nil }
 
--- writer/sql_test.go
+++ writer/sql_test.go
@@ -1,15 +1,41 @@
 package writer_test
 
 import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/agiledragon/gomonkey/v2"
 	"github.com/aureliano/db-unit-extractor/writer"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWriteHeader(t *testing.T) {
+func TestSQLWriteHeaderMkdirAllError(t *testing.T) {
+	patches := gomonkey.ApplyFunc(os.MkdirAll, func(string, fs.FileMode) error {
+		return fmt.Errorf("mkdir error")
+	})
+	defer patches.Reset()
+
 	w := writer.SQLWriter{}
-	assert.Nil(t, w.WriteHeader())
+
+	assert.Equal(t, "mkdir error", w.WriteHeader().Error())
+}
+
+func TestSQLWriteHeaderFileCreationError(t *testing.T) {
+	patches := gomonkey.ApplyFunc(os.OpenFile, func(string, int, fs.FileMode) (*os.File, error) {
+		return nil, fmt.Errorf("file creation error")
+	})
+	defer patches.Reset()
+
+	w := writer.SQLWriter{Directory: filepath.Join(os.TempDir(), "db-unit-extractor", "writer")}
+	assert.Equal(t, "file creation error", w.WriteHeader().Error())
+}
+
+func TestWriteHeader(t *testing.T) {
+	//w := writer.SQLWriter{}
+	//assert.Nil(t, w.WriteHeader())
 }
 
 func TestWriteFooter(t *testing.T) {
