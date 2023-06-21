f35105df5f76120406c723eb68f760983b7046e8
Aureliano <aureliano.franca@hotmail.com>
2023-06-21 20:24:54 -0300

    SQL writer: writes formatted and unformatted record.

--- writer/sql.go
+++ writer/sql.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/aureliano/db-unit-extractor/reader"
 )
 
 type SQLWriter struct {
@@ -35,4 +38,91 @@ func (w *SQLWriter) WriteFooter() error {
 	return w.file.Close()
 }
 
-func (w *SQLWriter) Write(table string, rows []map[string]interface{}) error { return nil }
+func (w *SQLWriter) Write(table string, rows [][]*reader.DBColumn) error {
+	if len(rows) == 0 {
+		return nil
+	}
+
+	content := sqlFileBody(w.Formatted, table, rows)
+	_, err := w.file.Write(content)
+	if err != nil {
+		log.Printf("SQL.Write\nWriting to file failed: `%s'\nContent: %s\n", err.Error(), content)
+		_ = w.file.Close()
+	}
+
+	return err
+}
+
+func sqlFileBody(formatted bool, table string, rows [][]*reader.DBColumn) []byte {
+	if formatted {
+		return formattedSQLRecord(table, rows)
+	}
+
+	return unformattedSQLRecord(table, rows)
+}
+
+func formattedSQLRecord(table string, rows [][]*reader.DBColumn) []byte {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("INSERT INTO %s(\n  ", table))
+
+	for i, cname := range rows[0] {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+
+		sb.WriteString(cname.Name)
+	}
+
+	sb.WriteString("\n) VALUES")
+	for _, row := range rows {
+		sb.WriteString("\n  (")
+		for i, column := range row {
+			if i > 0 {
+				sb.WriteString(", ")
+			}
+
+			if column.Value == nil {
+				sb.WriteString("null")
+			} else {
+				sb.WriteString(fmt.Sprintf("'%v'", column.Value))
+			}
+		}
+		sb.WriteString(")")
+	}
+	sb.WriteRune(';')
+
+	return []byte(sb.String())
+}
+
+func unformattedSQLRecord(table string, rows [][]*reader.DBColumn) []byte {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("insert into %s(", table))
+
+	for i, cname := range rows[0] {
+		if i > 0 {
+			sb.WriteRune(',')
+		}
+
+		sb.WriteString(cname.Name)
+	}
+
+	sb.WriteString(") values")
+	for _, row := range rows {
+		sb.WriteRune('(')
+		for i, column := range row {
+			if i > 0 {
+				sb.WriteRune(',')
+			}
+
+			if column.Value == nil {
+				sb.WriteString("null")
+			} else {
+				sb.WriteString(fmt.Sprintf("'%v'", column.Value))
+			}
+		}
+		sb.WriteRune(')')
+	}
+	sb.WriteRune(';')
+
+	return []byte(sb.String())
+}
--- writer/sql_test.go
+++ writer/sql_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/agiledragon/gomonkey/v2"
+	"github.com/aureliano/db-unit-extractor/reader"
 	"github.com/aureliano/db-unit-extractor/writer"
 	"github.com/stretchr/testify/assert"
 )
@@ -33,17 +34,98 @@ func TestSQLWriteHeaderFileCreationError(t *testing.T) {
 	assert.Equal(t, "file creation error", w.WriteHeader().Error())
 }
 
-func TestWriteHeader(t *testing.T) {
-	//w := writer.SQLWriter{}
-	//assert.Nil(t, w.WriteHeader())
-}
+func TestSQLWriteUnformattedEmptyData(t *testing.T) {
+	dir := filepath.Join(os.TempDir(), "db-unit-extractor", "writer")
+	w := writer.SQLWriter{
+		Formatted: false,
+		Directory: dir,
+		Name:      "test-write-unformatted",
+	}
 
-func TestWriteFooter(t *testing.T) {
-	w := writer.SQLWriter{}
+	assert.Nil(t, w.WriteHeader())
+
+	assert.Nil(t, w.Write("products", [][]*reader.DBColumn{}))
+
 	assert.Nil(t, w.WriteFooter())
+
+	bytes, _ := os.ReadFile(filepath.Join(dir, fmt.Sprintf("%s.sql", w.Name)))
+	assert.Empty(t, string(bytes))
 }
 
-func TestWrite(t *testing.T) {
-	w := writer.SQLWriter{}
-	assert.Nil(t, w.Write("", nil))
+func TestSQLWriteBodyFileWritingError(t *testing.T) {
+	patches := gomonkey.ApplyMethodFunc(&os.File{}, "Write", func([]byte) (int, error) {
+		return 0, fmt.Errorf("file writing error")
+	})
+	defer patches.Reset()
+
+	w := writer.SQLWriter{Directory: filepath.Join(os.TempDir(), "db-unit-extractor", "writer")}
+	assert.Equal(t, "file writing error", w.Write("", [][]*reader.DBColumn{{}}).Error())
+}
+
+func TestSQLWriteUnformatted(t *testing.T) {
+	dir := filepath.Join(os.TempDir(), "db-unit-extractor", "writer")
+	w := writer.SQLWriter{
+		Formatted: false,
+		Directory: dir,
+		Name:      "test-write-unformatted",
+	}
+
+	assert.Nil(t, w.WriteHeader())
+
+	rows := [][]*reader.DBColumn{{
+		{Name: "id", Value: 1},
+		{Name: "name", Value: "shirt"},
+		{Name: "description", Value: "black shirt"},
+		{Name: "price", Value: 14.50},
+	}, {
+		{Name: "id", Value: 2},
+		{Name: "name", Value: "pant"},
+		{Name: "description"},
+		{Name: "price", Value: 26.35},
+	}}
+
+	assert.Nil(t, w.Write("products", rows))
+
+	assert.Nil(t, w.WriteFooter())
+
+	bytes, _ := os.ReadFile(filepath.Join(dir, fmt.Sprintf("%s.sql", w.Name)))
+	actual := string(bytes)
+	expected := "insert into products(id,name,description,price) " +
+		"values('1','shirt','black shirt','14.5')('2','pant',null,'26.35');"
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestSQLWriteFormatted(t *testing.T) {
+	dir := filepath.Join(os.TempDir(), "db-unit-extractor", "writer")
+	w := writer.SQLWriter{
+		Formatted: true,
+		Directory: dir,
+		Name:      "test-write-unformatted",
+	}
+
+	assert.Nil(t, w.WriteHeader())
+
+	rows := [][]*reader.DBColumn{{
+		{Name: "id", Value: 1},
+		{Name: "name", Value: "shirt"},
+		{Name: "description", Value: "black shirt"},
+		{Name: "price", Value: 14.50},
+	}, {
+		{Name: "id", Value: 2},
+		{Name: "name", Value: "pant"},
+		{Name: "description"},
+		{Name: "price", Value: 26.35},
+	}}
+
+	assert.Nil(t, w.Write("products", rows))
+
+	assert.Nil(t, w.WriteFooter())
+
+	bytes, _ := os.ReadFile(filepath.Join(dir, fmt.Sprintf("%s.sql", w.Name)))
+	actual := string(bytes)
+	expected := "INSERT INTO products(\n  id, name, description, price\n) " +
+		"VALUES\n  ('1', 'shirt', 'black shirt', '14.5')\n  ('2', 'pant', null, '26.35');"
+
+	assert.Equal(t, expected, actual)
 }
