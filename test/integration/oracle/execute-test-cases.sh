388cbc24ccbe4417ea80a5376a66910a84f83267
Aureliano <aureliano.franca@hotmail.com>
2023-06-25 12:52:24 -0300

    85 create an integration test that uses template (#87)
    * Creates a template to regions domain.
    
    * Uses template of regions domain.
    
    * Change integration tests execution order: unformatted then formatted writers.

--- test/integration/oracle/execute-test-cases.sh
+++ test/integration/oracle/execute-test-cases.sh
@@ -8,40 +8,6 @@ echo
 
 echo "Oracle database"
 echo
-echo "Scenario: Formatted employees data-set"
-
-dist/db-unit-extractor_linux_amd64_v1/db-unit-extractor extract \
-    -n oracle://sys:admin@localhost:1521/xe \
-    -s test/integration/oracle/models/employees-ds-model.yml \
-    -t xml -t sql -f \
-    -r employee_id=200 \
-    -d /tmp/db-unit-extractor/integration-tests/oracle
-
-echo "  Check XML data-set"
-go run test/integration/*.go \
-    test/integration/oracle/expectations/employees-ds-expectation.yml \
-    /tmp/db-unit-extractor/integration-tests/oracle/employees-ds-model.xml
-
-if [ $? -ne 0 ];
-then
-    echo
-    echo " [ERROR] Check XML data-set failed."
-    exit 1
-fi
-
-echo "  Check SQL data-set"
-go run test/integration/*.go \
-    test/integration/oracle/expectations/employees-ds-expectation.yml \
-    /tmp/db-unit-extractor/integration-tests/oracle/employees-ds-model.sql
-
-if [ $? -ne 0 ];
-then
-    echo
-    echo " [ERROR] Check SQL data-set failed."
-    exit 1
-fi
-
-echo
 
 echo "Scenario: Unformatted employees data-set"
 dist/db-unit-extractor_linux_amd64_v1/db-unit-extractor extract \
@@ -77,11 +43,46 @@ fi
 
 echo
 
-echo "Scenario: Formatted departments data-set"
+echo "Scenario: Formatted employees data-set"
+
+dist/db-unit-extractor_linux_amd64_v1/db-unit-extractor extract \
+    -n oracle://sys:admin@localhost:1521/xe \
+    -s test/integration/oracle/models/employees-ds-model.yml \
+    -t xml -t sql -f \
+    -r employee_id=200 \
+    -d /tmp/db-unit-extractor/integration-tests/oracle
+
+echo "  Check XML data-set"
+go run test/integration/*.go \
+    test/integration/oracle/expectations/employees-ds-expectation.yml \
+    /tmp/db-unit-extractor/integration-tests/oracle/employees-ds-model.xml
+
+if [ $? -ne 0 ];
+then
+    echo
+    echo " [ERROR] Check XML data-set failed."
+    exit 1
+fi
+
+echo "  Check SQL data-set"
+go run test/integration/*.go \
+    test/integration/oracle/expectations/employees-ds-expectation.yml \
+    /tmp/db-unit-extractor/integration-tests/oracle/employees-ds-model.sql
+
+if [ $? -ne 0 ];
+then
+    echo
+    echo " [ERROR] Check SQL data-set failed."
+    exit 1
+fi
+
+echo
+
+echo "Scenario: Unformatted departments data-set"
 dist/db-unit-extractor_linux_amd64_v1/db-unit-extractor extract \
     -n oracle://sys:admin@localhost:1521/xe \
     -s test/integration/oracle/models/departments-ds-model.yml \
-    -t xml -t sql -f \
+    -t xml -t sql \
     -r department_id=90 \
     -d /tmp/db-unit-extractor/integration-tests/oracle
 
@@ -111,11 +112,11 @@ fi
 
 echo
 
-echo "Scenario: Unformatted departments data-set"
+echo "Scenario: Formatted departments data-set"
 dist/db-unit-extractor_linux_amd64_v1/db-unit-extractor extract \
     -n oracle://sys:admin@localhost:1521/xe \
     -s test/integration/oracle/models/departments-ds-model.yml \
-    -t xml -t sql \
+    -t xml -t sql -f \
     -r department_id=90 \
     -d /tmp/db-unit-extractor/integration-tests/oracle
 
